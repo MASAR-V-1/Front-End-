cbcd9ab47bc5f08d8fdbbac308ec9a5f505b675f
MahmoudSalem322 <20220766@student.alazhar.edu.ps.com>
2026-06-17 11:21:29 +0300

    add new registration component

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Security from "./components/Security/Security";
+import Registration from "./components/Security/Registration";
 
 const page = () => {
   return (
@@ -10,6 +11,7 @@ const page = () => {
       <Navbar />
       <Hero />
       <Security />
+      <Registration />
     </>
   );
 };
